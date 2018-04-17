4991a75a50390644b7342f89a8ec0565771bc97d
jthomaz <jthomaz@brasilcap.com.br>
2018-04-17 16:23:28 -0300

    Criando acesso ao Redis

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var SNS = require('./Services/notifications/SNS');
 var SQS = require('./Services/notifications/SQS');
-var Mencached = require('./Services/cache/Memcached');
+var Redis = require('./Services/cache/Redis');
 
 exports.SNS_Post = function (snsURL, payload, subject, region, callback){
     SNS_Post = new SNS(region);
@@ -29,25 +29,25 @@ exports.SQS_Delete = function (queueURL, receiptHandle, region, callback){
 
 exports.Memcached_Get = function (key, host, port, callback){
 
-    Memcached_Get = new Mencached(host, port);
+    Memcached_Get = new Redis(host, port);
     
     Memcached_Get.get(key, function(err, data){
         callback(err, data);
     });
 }
 
-exports.Memcached_Post = function (key, value, host, port, callback){
+exports.Memcached_Post = function (key, value, ttl, host, port, callback){
 
-    Memcached_Post = new Mencached(host, port);
+    Memcached_Post = new Redis(host, port);
     
-    Memcached_Post.post(key, value, function(err, data){
+    Memcached_Post.post(key, value, ttl, function(err, data){
         callback(err, data);
     });
 }
 
 exports.Memcached_Delete = function (key, host, port, callback){
 
-    Memcached_Delete = new Mencached(host, port);
+    Memcached_Delete = new Redis(host, port);
     
     Memcached_Delete.delete(key, function(err, data){
         callback(err, data);
--- test/testRedis.js
+++ test/testRedis.js
@@ -10,7 +10,7 @@ BRCAPAWS.Memcached_Get('Framework', 'brasilcap-dev.i8cxyw.ng.0001.sae1.cache.ama
 });
 
 
-BRCAPAWS.Memcached_Post('Framework', 'Angular', 'brasilcap-dev.i8cxyw.ng.0001.sae1.cache.amazonaws.com', '6379', function(err, data){
+BRCAPAWS.Memcached_Post('Framework', 'Angular', 1, 'brasilcap-dev.i8cxyw.ng.0001.sae1.cache.amazonaws.com', '6379', function(err, data){
     if (err){
         console.log(err);
     }else{
